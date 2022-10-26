4b883a020eb354014b4dd83182edd82badf0f53c
Kritika Taank <taank.kritika@gmail.com>
2022-10-26 15:04:54 +0530

    Update main.py

--- 52 ISS Overhead Notifier/main.py
+++ 52 ISS Overhead Notifier/main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "viola.thebookaliciousmess@gmail.com"
-MY_PASSWORD = "Adhd24*7"
+MY_EMAIL = "your email"
+MY_PASSWORD = "your password"
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
 
